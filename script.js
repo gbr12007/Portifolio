01348d7a748e7c380963677a7fba2bf658cf63c3
Gabriel Siqueira <107729757+gbr12007@users.noreply.github.com>
2025-01-13 01:01:00 -0300

    Modo dark

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const bola = document.querySelector('.bola');
 const trilha = document.querySelector('.trilha');
 const home = document.querySelector('#home');
+const nav = document.querySelector('.nav');
+const social = document.querySelector('.social');
 
 trilha.addEventListener('click', ()=>{
     trilha.classList.toggle('dark');
     document.body.classList.toggle('dark');
     home.classList.toggle('dark');
+    nav.classList.toggle('dark');
+    social.classList.toggle('dark');
 })
